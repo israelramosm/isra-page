d54aaa0c2c07b718ea65d32f4cf363263d4b2524
Israel Ramos <arnulfo_israel@hotmail.com>
2024-07-11 20:27:24 -0600

    chore(config):  config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
-const pathPrefix = process.env.NODE_ENV === "production" ? "/isra-page" : "";
 
-const nextConfig = {
-  output: "export",
-  assetPrefix: pathPrefix,
-  basepath: pathPrefix,
-};
+const nextConfig = {};
 
 export default nextConfig;
